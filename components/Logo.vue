53618ab00ae87a6e486ca9c065a2dbdf133f6926
Ondřej Čermák <ondrej.l.cermak@gmail.com>
2020-01-22 21:35:39 +0100

    Initial commit

---
+++ components/Logo.vue
@@ -0,0 +1,79 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="70.485pt"
+    width="523.8pt"
+    viewBox="0 0 349.2 46.99"
+    class="randef-logo"
+  >
+    <title>RANDEF logo</title>
+    <g id="Layer_1">
+      <path
+        class="color-secondary"
+        d="M160.77,0V47H158l-32-40.81V47h-3.42V0h2.88l32,40.82V0Z"
+      />
+      <path
+        class="color-primary"
+        d="M228.3,33.58C224.88,39.93,218,43.9,208.9,43.9H194V3.09h14.9c9.49,0,16.53,4.32,19.79,11.14h3.71C228.91,5.59,220.42,0,209.17,0h-18.6V47h18.6c10.89,0,19.18-5.25,22.86-13.41Z"
+      />
+      <polygon
+        class="color-primary"
+        points="262.87 14.23 262.87 3.09 290.86 3.09 290.86 0 259.45 0 259.45 14.23 262.87 14.23"
+      />
+      <polygon
+        class="color-primary"
+        points="262.87 33.58 259.45 33.58 259.45 46.99 291.87 46.99 291.87 43.9 262.87 43.9 262.87 33.58"
+      />
+      <rect
+        class="color-primary"
+        x="317.78"
+        y="33.58"
+        width="3.42"
+        height="13.41"
+      />
+      <polygon
+        class="color-primary"
+        points="317.78 0 317.78 14.23 321.2 14.23 321.2 3.09 349.2 3.09 349.2 0 317.78 0"
+      />
+      <circle class="color-primary" cx="230.84" cy="23.5" r="4.85" />
+      <circle class="color-primary" cx="319.49" cy="23.5" r="4.85" />
+      <rect
+        class="color-primary"
+        x="232.4"
+        y="22.17"
+        width="85.38"
+        height="2.62"
+      />
+      <polygon
+        class="color-secondary"
+        points="78.94 0 75.52 0 53.9 46.99 57.59 46.99 63.57 33.77 64.91 30.88 77.2 3.76 89.55 30.88 90.82 33.77 96.86 46.99 100.56 46.99 78.94 0"
+      />
+      <path
+        class="color-secondary"
+        d="M24.77,30.54c7-2.08,11-7.25,11-14.7C35.78,5.91,28.66,0,16.85,0H0V47H3.42V3.09H16.85c10.07,0,15.44,4.63,15.44,12.75,0,7.31-4.43,11.84-12.76,12.62v0l1.87,2.8L32.62,47h3.9Z"
+      />
+    </g>
+  </svg>
+</template>
+<style>
+.randef-logo {
+  animation: 1s appear;
+}
+
+.color-primary {
+  fill: #0f9bab;
+}
+
+.color-secondary {
+  fill: #333;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <div class="links">
+        <a
+          href="https://www.linkedin.com/in/0ndrejcermak/"
+          target="_blank"
+          class="button social linkedin"
+        >
+          <fa :icon="fab.faLinkedinIn" />
+        </a>
+        <a
+          href="https://github.com/randef-com"
+          target="_blank"
+          class="button social github"
+        >
+          <fa :icon="fab.faGithub" />
+        </a>
+        <a
+          href="https://www.instagram.com/oonacermak/"
+          target="_blank"
+          class="button social instagram"
+        >
+          <fa :icon="fab.faInstagram" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  computed: {
+    fab() {
+      return fab
+    },
+    fas() {
+      return fas
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.links {
+  padding-top: 45px;
+  font-size: 22px;
+}
+.linkedin {
+  border: 1px solid #2867b2;
+  color: #2867b2;
+}
+
+.linkedin:hover {
+  color: #fff;
+  background-color: #2867b2;
+}
+
+.github {
+  border: 1px solid #211f1f;
+  color: #211f1f;
+}
+
+.github:hover {
+  color: #fff;
+  background-color: #211f1f;
+}
+
+.instagram {
+  border: 1px solid #c13584;
+  color: #c13584;
+}
+
+.instagram:hover {
+  color: #fff;
+  background-color: #c13584;
+}
+
+.social {
+  margin: 15px;
+}
+</style>
